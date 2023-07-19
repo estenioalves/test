b3856b6dbe434fb1deb64d490b5367654ebac9c8
estenioalves <estenioalves94@gmail.com>
2023-07-19 19:54:04 -0300

    Remodelando no padrão page object

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -1,18 +1,17 @@
+import Cadastro from '../support/pages/cadastro/pagina-cadastro'
+
 describe('Página de cadastro', () => {
-  beforeEach(() => {
+
+    beforeEach(() => {
       cy.visit('http://localhost:4200/')
-  })
-
-  const usuarios = require('../fixtures/usuarios.json')
-
-  usuarios.forEach(usuario => {
-      it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
-          cy.get('[data-test="register"]').click();
-          cy.get('input[data-test="email"]').type(usuario.email);
-          cy.get('input[data-test="fullName"]').type(usuario.fullName);
-          cy.get('input[data-test="registerUserName"]').type(usuario.userName);
-          cy.get('input[data-test="registerPassword"]').type(usuario.password);
-          cy.get('[data-test="btnRegister"]').click();
-      })
-  })
-})
+    })
+        
+        it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+          
+          Cadastro.acessarPaginaDeCadastro();
+          Cadastro.preencherFormulario();
+          Cadastro.submeterCadastro()
+          
+        })
+    })
+  
